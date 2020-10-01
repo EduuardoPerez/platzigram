610da879d5c19cffa5a85e0439bd713f20572e2a
Eduardo PÃerez <eperez@prismamp.com>
2020-09-30 23:04:42 -0300

    It was created a script for execute the virtual enviroment and the DJango basic commands according to arguments passed

---
+++ djangoCommands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# alias penv="source ../venv/Scripts/activate"
+# alias runserver="py manage.py runserver"
+# alias migrate="py manage.py migrate"
+# alias makemigrations="py manage.py makemigrations"
+
+if [ -z $1 ]
+then
+  echo "Type an argument. Posible arg: env run migrate mkmigrations"
+fi
+
+COMMAND=${1}
+
+case $COMMAND in
+	"env")
+		# For make this command run the djangoCommands.sh script has to be executed in with source
+		source ../venv/Scripts/activate;;
+	"run")
+		py manage.py runserver;;
+	"migrate")
+		py manage.py migrate;;
+	"mkmigrations")
+		py manage.py makemigrations;;
+	*)
+		echo "The command is not supported";;
+esac
