a1170566150ee1d1d5942c9e32b34146ad5f12cb
Eduardo PÃerez <eperez@prismamp.com>
2020-10-13 20:22:53 -0300

    The firts migration was made it

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
+""" Posts models """
+
+# Django
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  """ User model """
+
+  email = models.EmailField(unique=True)
+  password = models.CharField(max_length=12)
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  bio = models.TextField(blank=True)
+  birthday = models.DateField(blank=True, null=True)
+  created_at = models.DateTimeField(auto_now_add=True) # When an instace is created in the DB, the datetime it's goint to be passed
+  modify_at = models.DateTimeField(auto_now=True) # When an instace is modified in the DB, the datetime it's goint to be updated
